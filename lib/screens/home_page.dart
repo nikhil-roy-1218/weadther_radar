9574aa63c17a2da6520efeb951a227ef71a48648
Nikhil Prem <nikhilprem0210@gmail.com>
2025-07-17 15:41:57 +0530

    add home wiget with app bar

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Weadther App",
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.red.shade100,
+          ),
+        ),
+      ),
+    );
+  }
+}
