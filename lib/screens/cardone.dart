5d45b9bf6f1a1284582b9f0888f85bfdb5431ec2
Nikhil Prem <nikhilprem0210@gmail.com>
2025-07-22 17:22:34 +0530

    Extract Card widget to  HourlyForecastitem

--- lib/screens/cardone.dart
+++ lib/screens/cardone.dart
@@ -62,165 +62,44 @@ class FirsrCard extends StatelessWidget {
               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                SizedBox(
-                  width: 160,
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    elevation: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00 AM",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 48, color: Colors.white),
-                          SizedBox(height: 12),
-
-                          Text(
-                            "301.17",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 160,
-                  child: Card(
-                    elevation: 1,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00 AM",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 48, color: Colors.white),
-                          SizedBox(height: 12),
-
-                          Text(
-                            "301.17",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 160,
-                  child: Card(
-                    elevation: 1,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00 AM",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 48, color: Colors.white),
-                          SizedBox(height: 12),
-
-                          Text(
-                            "301.17",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 160,
-                  child: Card(
-                    elevation: 1,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00 AM",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 48, color: Colors.white),
-                          SizedBox(height: 12),
-
-                          Text(
-                            "301.17",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 160,
-                  child: Card(
-                    elevation: 1,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        children: [
-                          Text(
-                            "9:00 AM",
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 48, color: Colors.white),
-                          SizedBox(height: 12),
-
-                          Text(
-                            "301.17",
-                            style: TextStyle(fontSize: 24, color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                // Add some space at the start
+                SizedBox(width: 160, child: HourlyForecastitem()),
+                SizedBox(width: 160, child: HourlyForecastitem()),
+                SizedBox(width: 160, child: HourlyForecastitem()),
+                SizedBox(width: 160, child: HourlyForecastitem()),
+                SizedBox(width: 160, child: HourlyForecastitem()),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class HourlyForecastitem extends StatelessWidget {
+  const HourlyForecastitem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              "9:00 AM",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Icon(Icons.cloud, size: 48, color: Colors.white),
+            SizedBox(height: 12),
+
+            Text("301.17", style: TextStyle(fontSize: 24, color: Colors.white)),
+          ],
+        ),
       ),
     );
   }
